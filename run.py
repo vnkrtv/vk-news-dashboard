21b7f1906ca9c6286d9ef850a25fa0fc58b96b52
LeadNess <leadness@mail.ru>
2020-08-18 10:08:07 +0300

    Update web engine
    Moved from developing web server to gunicorn

--- run.py
+++
@@ -1,3 +0,0 @@
-import app.app as application
-
-application.app.run_server(host='0.0.0.0', debug=True)
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+import app.app as application
+
+application.app.run_server(host='0.0.0.0', debug=False)
